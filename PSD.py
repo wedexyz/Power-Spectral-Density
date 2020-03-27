0371ac9caf36da117f022da820482ea998255cc6
wedexyz <38834987+wedexyz@users.noreply.github.com>
2020-03-27 23:49:40 +0700

    Update PSD.py

--- PSD.py
+++ PSD.py
@@ -2,12 +2,12 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-# Seed the random number generator
+# data ramdom 
 np.random.seed(0)
 time_step = .01
 time_vec = np.arange(0, 70, time_step)
 
-# A signal with a small frequency chirp
+# signal
 sig = np.sin(0.5 * np.pi * time_vec * (1 + .1 * time_vec))
 
 plt.figure(figsize=(8, 5))
